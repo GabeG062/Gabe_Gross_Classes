b726aad42fa460846e113544e7b6c9d16b4ce73f
starman062 <gabrielgross222@gmail.com>
2026-05-09 20:49:21 -0700

    Finished Customer.cs

--- classeslab_master/ClassesLab_Core5/BlackJack/BlackJack/obj/Debug/net10.0/BlackJack.AssemblyInfo.cs
+++ classeslab_master/ClassesLab_Core5/BlackJack/BlackJack/obj/Debug/net10.0/BlackJack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlackJack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+557eeebd496951a9456d2b2b28c82ec2ee393ddc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9453c7ccac912424923e613c2a4e80db57f6f83d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlackJack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlackJack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- classeslab_master/ClassesLab_Core5/CustomerProductSolution/CustomerProductClasses/Customer.cs
+++ classeslab_master/ClassesLab_Core5/CustomerProductSolution/CustomerProductClasses/Customer.cs
@@ -4,5 +4,87 @@ namespace CustomerProductClasses
 {
     public class Customer
     {
+        private int id;
+        private string first;
+        private string last;
+        private string mail;
+        private string phoneNum;
+
+        public Customer() { }
+
+        public Customer(string email, string firstName, int customerId, string lastName, string phone )
+        {
+            Id = customerId;
+            Email = email;
+            FirstName = firstName;
+            LastName = lastName;
+            Phone = phone;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return mail;
+            }
+            set
+            {
+                mail = value;
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return first;
+            }
+            set
+            {
+                first = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return last;
+            }
+            set
+            {
+                last = value;
+            }
+        }
+
+        public string Phone
+        {
+            get
+            {
+                return phoneNum;
+            }
+            set
+            {
+                phoneNum = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Id: {0} Email: {1} Firstname: {2} Lastname: {3} Phone: {4}", id, Email, FirstName, LastName, Phone);
+        }
     }
 }
+
